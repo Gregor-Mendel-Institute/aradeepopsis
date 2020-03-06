39bb3673f1e1eb45b35fc179b2e25f38a6a41d8e
phue <patrick.huether@gmi.oeaw.ac.at>
2020-03-06 16:56:50 +0100

    shiny app: add option to choose date format
    also sanitize filenames to remove path and extension

--- assets/shiny/app.R
+++ assets/shiny/app.R
@@ -2,13 +2,12 @@ library(shiny)
 library(slickR)
 library(tidyverse)
 library(radarchart)
-library(glue)
-library(lubridate)
 library(shinycssloaders)
 library(shinythemes)
 
 data <- read_csv("aradeepopsis_traits.csv")
 imagenames <- data %>% select(file)
+dateformats <- c('%d-%m','%m-%d','%d-%m-%y','%m-%d-%y','%y-%m-%d','%y-%d-%m')
 
 traitcount <- ncol(data) - 2 #filename and extension don't count
 imagecount <- nrow(data)
@@ -39,8 +38,8 @@ ui <- navbarPage(title="araDeepopsis", theme = shinytheme("flatly"),
 						condition="input.tabset2 > 0",
 						selectizeInput("statistics_traits","Select Trait:", choices = colnames(data %>% select(-file,-format)), selected = "rosette"),
 						conditionalPanel(
-						condition="input.tabset2 == 2",
-						selectizeInput("statistics_files",label="Select Image:", choices=NULL)
+							condition="input.tabset2 == 2",
+							selectizeInput("statistics_files",label="Select Image:", choices=NULL)
 						)
 					)
 				),
@@ -53,12 +52,15 @@ ui <- navbarPage(title="araDeepopsis", theme = shinytheme("flatly"),
 		),
 		tabPanel("Rosette Experiment",
 				sidebarPanel(
-					fileInput("metadata",label = "Select file"),
-					tableOutput("meta"),
-					varSelectInput("file","Select column containing the filename", data = NULL),
-					varSelectInput("date","Select column containing the date", data = NULL),
-					varSelectInput("id","Select column containing grouping variable", data = NULL),
-					actionButton("mergedat", "Merge data"),
+					fileInput("metadata_table",label = "Select file"),
+					div(style = "overflow-x:scroll;", tableOutput("meta")),
+					varSelectizeInput("file","Select column containing the filename", data = NULL),
+					splitLayout(cellArgs = list(style = "overflow:visible;"),
+						varSelectizeInput("date","Select column containing the date", data = NULL),
+						selectizeInput("date_format","Select format",choices=NULL)
+					),
+					varSelectizeInput("groupvar","Select column to group by", data = NULL),
+					actionButton("merge_data", "Merge data"),
 					selectizeInput("exp_traits","Select Trait:", choices = colnames(data %>% select(-file,-format)), selected = "rosette")
 				),
 				mainPanel(
@@ -70,98 +72,94 @@ ui <- navbarPage(title="araDeepopsis", theme = shinytheme("flatly"),
 )
 
 server <- function(input, output, session) {
-    #for large datasets it helps with performance if selection lists are done on the server-side
-    updateSelectizeInput(session, "statistics_files", choices = c(imagenames), server = TRUE)
-    updateSelectizeInput(session, "explorer_files", choices = c(imagenames), server = TRUE)
-    
-    output$mask <- renderImage({
-        list(src = glue("diagnostics/single_pot/mask/{input$explorer_files}.png"),width=400,height=400)
-    }, deleteFile = FALSE)
-    output$rosette <- renderImage({
-        list(src = glue("diagnostics/single_pot/crop/{input$explorer_files}.jpeg"),width=400,height=400)
-    }, deleteFile = FALSE)
-    output$overlay <- renderImage({
-        list(src = glue("diagnostics/single_pot/overlay/{input$explorer_files}.jpeg"),width=400,height=400)
-    }, deleteFile = FALSE)
-    output$radar = renderChartJSRadar({
-        data %>%
-            filter(file == input$explorer_files) %>%
-            select(one_of(c("rosette","anthocyanin","senescent"))) %>% 
-            pivot_longer(everything(),names_to = "Label") %>% 
-            mutate(value=value/sum(value)*100) %>% 
-            chartJSRadar(.,maxScale = 100,scaleStartValue = 0,scaleStepWidth = 25,showLegend = F)
-        
-    })
-    output$histograms = renderPlot(width=400,{
-        data %>%
-            select(file,input$statistics_traits) %>%
-            ggplot(aes_string(input$statistics_traits)) +
-            geom_histogram(bins = 100) +
-            theme_bw()
-    })
-    output$jitter = renderPlot(width=400,{
-        data %>%
-            select(file,input$statistics_traits) %>%
-            ggplot(aes_string(x=as.factor(input$statistics_traits),y=input$statistics_traits)) +
-            geom_jitter(colour="black",alpha=0.3,size=5) +
-            geom_jitter(data = {. %>% filter(file==input$statistics_files)}, colour="red", size=5) +
-            xlab(element_blank()) +
-            ylab("measurement") +
-            theme_bw()
-    })
-    output$meta = renderTable({
-        filedata() %>% head()
-    })
-    
-    filedata <- eventReactive (input$metadata,{
-        metafile <- input$metadata
-        req(metafile)
-        table <- read_csv(metafile$datapath)
-        columns = colnames(table)
-        updateselectizeInput(session, "file", choices = columns)
-        #TODO handle different date formats
-        updateselectizeInput(session, "date", choices = columns)
-        updateselectizeInput(session, "id", choices = columns)
-        table
-    })
-    
-    joined <- eventReactive(input$mergedat,{
-        data %>%
-            rename(!!input$file := file) %>%
-            left_join(.,filedata(),by=as.character(input$file)) %>%
-            mutate(date = !!input$date %>% mdy(.,truncated = 1)) %>%
-            group_by(!!input$id)
-    })
-    
-    output$merged = renderTable({
-        joined() %>% head()
-    })
-    output$timeline = renderPlot({
-        joined() %>% ggplot(aes_string(x='date',y=input$exp_traits,colour=quo(as.factor(!!input$id)))) + #this seems wrong but works
-            stat_summary() +
-            theme_bw()
-    })
-    
-    output$info = renderText({
-        glue("Measured {traitcount} traits across {imagecount} rosettes")
-    })
-    output$slickr <- renderSlickR({
-        
-        # split the filenames into chunks of 60, corresponding to 10 pages per chunk
-        # this drastically improves page loading time
-        chunks <- if (imagecount > 60) split(imagenames$file, ceiling(seq_along(imagenames$file)/60)) else split(imagenames$file, 1)
-        
-        opts <- settings(slidesToShow = 6, slidesToScroll = 6)
-        
-        overlay <- slickR(glue("diagnostics/single_pot/overlay/{chunks[[input$chunk]]}.jpeg"), height = 200) + opts
-        mask <- slickR(glue("diagnostics/single_pot/mask/{chunks[[input$chunk]]}.png"), height = 200) + opts + settings(arrows = F)
-        crop <- slickR(glue("diagnostics/single_pot/crop/{chunks[[input$chunk]]}.jpeg"), height = 200) + opts + settings(arrows = F)
-        names <- slickR(as.character(chunks[[input$chunk]]), slideType = 'p') + opts + settings(arrows = F)
-        overlay %synch% (crop %synch% (mask %synch% names))
-    })
-    session$onSessionEnded(function() {
-        stopApp()
-    })
+		#for large datasets it helps with performance if selection lists are done on the server-side
+		updateSelectizeInput(session, "statistics_files", choices = c(imagenames), server = TRUE)
+		updateSelectizeInput(session, "explorer_files", choices = c(imagenames), server = TRUE)
+
+		output$mask <- renderImage({
+			list(src = glue::glue("diagnostics/single_pot/mask/{input$explorer_files}.png"),width=400,height=400)
+		}, deleteFile = FALSE)
+		output$rosette <- renderImage({
+			list(src = glue::glue("diagnostics/single_pot/crop/{input$explorer_files}.jpeg"),width=400,height=400)
+		}, deleteFile = FALSE)
+		output$overlay <- renderImage({
+			list(src = glue::glue("diagnostics/single_pot/overlay/{input$explorer_files}.jpeg"),width=400,height=400)
+		}, deleteFile = FALSE)
+		output$radar = renderChartJSRadar({
+			data %>%
+				filter(file == input$explorer_files) %>%
+				select(one_of(c("rosette","anthocyanin","senescent"))) %>% 
+				pivot_longer(everything(),names_to = "Label") %>% 
+				mutate(value=value/sum(value)*100) %>% 
+				chartJSRadar(.,maxScale = 100,scaleStartValue = 0,scaleStepWidth = 25,showLegend = F)
+		})
+		output$histograms = renderPlot(width=400,{
+			data %>%
+				select(file,input$statistics_traits) %>%
+				ggplot(aes_string(input$statistics_traits)) +
+				geom_histogram(bins = 100) +
+				theme_bw()
+		})
+		output$jitter = renderPlot(width=400,{
+			data %>%
+				select(file,input$statistics_traits) %>%
+				ggplot(aes_string(x=as.factor(input$statistics_traits),y=input$statistics_traits)) +
+				geom_jitter(colour="black",alpha=0.3,size=5) +
+				geom_jitter(data = {. %>% filter(file==input$statistics_files)}, colour="red", size=5) +
+				xlab(element_blank()) +
+				ylab("measurement") +
+				theme_bw()
+		})
+		output$meta = renderTable(striped = TRUE,width="100px",{
+			filedata() %>% head(n=1)
+		})
+
+		filedata <- eventReactive(input$metadata_table,{
+			metafile <- input$metadata_table
+			req(metafile)
+			table <- read_csv(metafile$datapath)
+			columns = colnames(table)
+			updateSelectizeInput(session, "file", choices = columns)
+			updateSelectizeInput(session, "date", choices = columns)
+			updateSelectizeInput(session, "date_format", choices = dateformats)
+			updateSelectizeInput(session, "groupvar", choices = columns)
+			table
+		})
+		joined <- eventReactive(input$merge_data,{
+			filedata() %>%
+				rename(file := !!input$file) %>% 
+				mutate_at(.,.vars=vars(file),.funs=~tools::file_path_sans_ext(basename(.))) %>% 
+				right_join(.,data) %>% 
+				mutate(date = !!input$date %>% lubridate::parse_date_time(.,orders = input$date_format)) %>% 
+				group_by(!!input$groupvar)
+		})
+		output$timeline = renderPlot({
+			joined() %>% ggplot(aes_string(x='date',y=input$exp_traits,colour=quo(as.factor(!!input$groupvar)))) +
+				stat_summary() +
+				scale_color_viridis_d() +
+				theme_bw()
+		})
+
+		output$info = renderText({
+			glue::glue("Measured {traitcount} traits across {imagecount} rosettes")
+		})
+		output$slickr <- renderSlickR({
+			
+			# split the filenames into chunks of 60, corresponding to 10 pages per chunk
+			# this drastically improves page loading time
+			chunks <- if (imagecount > 60) split(imagenames$file, ceiling(seq_along(imagenames$file)/60)) else split(imagenames$file, 1)
+			
+			opts <- settings(slidesToShow = 6, slidesToScroll = 6)
+			
+			overlay <- slickR(glue::glue("diagnostics/single_pot/overlay/{chunks[[input$chunk]]}.jpeg"), height = 200) + opts
+			mask <- slickR(glue::glue("diagnostics/single_pot/mask/{chunks[[input$chunk]]}.png"), height = 200) + opts + settings(arrows = F)
+			crop <- slickR(glue::glue("diagnostics/single_pot/crop/{chunks[[input$chunk]]}.jpeg"), height = 200) + opts + settings(arrows = F)
+			names <- slickR(as.character(chunks[[input$chunk]]), slideType = 'p') + opts + settings(arrows = F)
+			overlay %synch% (crop %synch% (mask %synch% names))
+		})
+		session$onSessionEnded(function() {
+			stopApp()
+		})
 }
 
 # Run the application 
