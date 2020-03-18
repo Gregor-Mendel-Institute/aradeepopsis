e0f0fb97b24a8312f460ae477d5758c4adfafb84
phue <patrick.huether@gmail.com>
2020-03-19 00:50:42 +0100

    code formatting changes

--- assets/shiny/app.R
+++ assets/shiny/app.R
@@ -17,96 +17,96 @@ imagecount <- nrow(data)
 # Define UI
 ui <- navbarPage(title="araDeepopsis", id="nav", theme = shinytheme("flatly"),
 		tabPanel("Rosette Carousel",
-				sliderInput("chunk", label = "Select chunk:", min = 1, max = ceiling(imagecount/60), value = 1, width = '100%', step = 1),
-				slickROutput("slickr",width='100%',height='400px') %>% withSpinner()
+			sliderInput("chunk", label = "Select chunk:", min = 1, max = ceiling(imagecount/60), value = 1, width = '100%', step = 1),
+			slickROutput("slickr",width='100%',height='400px') %>% withSpinner()
 		),
 		tabPanel("Rosette Explorer",
-				sidebarPanel(
-					selectizeInput("explorer_files",label="Select Image:", choices=NULL)
+			sidebarPanel(
+				selectizeInput("explorer_files",label="Select Image:", choices=NULL)
+			),
+			mainPanel(
+				tabsetPanel(id='tabset1',type='pills',
+					tabPanel("Overlay",value=0,imageOutput("overlay")),
+					tabPanel("Mask",value=0,imageOutput("mask")),
+					tabPanel("Rosette",value=0,imageOutput("rosette")),
+					tabPanel("Convex Hull",value=0,imageOutput("hull")),
+					tabPanel("Leaf Classification",value=0,chartJSRadarOutput("radar", height = "200") %>% withSpinner())
 				),
-				mainPanel(
-					tabsetPanel(id='tabset1',type='pills',
-								tabPanel("Overlay",value=0,imageOutput("overlay")),
-								tabPanel("Mask",value=0,imageOutput("mask")),
-								tabPanel("Rosette",value=0,imageOutput("rosette")),
-								tabPanel("Convex Hull",value=0,imageOutput("hull")),
-								tabPanel("Leaf Classification",value=0,chartJSRadarOutput("radar", height = "200"))
-					),
-				),
+			),
 		),
 		tabPanel("Rosette Statistics",
-				sidebarPanel(
-					verbatimTextOutput("info"),
-					conditionalPanel(
-						condition="input.tabset2 == 0",
-						selectizeInput("correlations_type",label="Select Covariance method:", choices=c("pairwise.complete.obs", "complete.obs", "everything", "all.obs", "na.or.complete"))
-					),
-					conditionalPanel(
-						condition="input.tabset2 > 0",
-						selectizeInput("statistics_traits","Select Trait:", choices = colnames(data %>% select(-file,-format)), selected = "rosette"),
-						conditionalPanel(
-							condition="input.tabset2 == 2",
-							selectizeInput("statistics_files",label="Select Image:", choices=NULL)
-						),
-					)
+			sidebarPanel(
+				verbatimTextOutput("info"),
+				conditionalPanel(
+					condition="input.tabset2 == 0",
+					selectizeInput("correlations_type",label="Select Covariance method:", choices=c("pairwise.complete.obs", "complete.obs", "everything", "all.obs", "na.or.complete"))
 				),
-				mainPanel(
-					tabsetPanel(id='tabset2',type='pills',
-								tabPanel("Trait Correlation",value=0,plotOutput("correlations") %>% withSpinner()),
-								tabPanel("Trait Histogram",value=1,plotOutput("histograms") %>% withSpinner()),
-								tabPanel("Trait Jitterplot",value=2,plotOutput("jitter") %>% withSpinner())
-					)
+				conditionalPanel(
+					condition="input.tabset2 > 0",
+					selectizeInput("statistics_traits","Select Trait:", choices = colnames(data %>% select(-file,-format)), selected = "rosette"),
+					conditionalPanel(
+						condition="input.tabset2 == 2",
+						selectizeInput("statistics_files",label="Select Image:", choices=NULL)
+					),
 				)
+			),
+			mainPanel(
+				tabsetPanel(id='tabset2',type='pills',
+					tabPanel("Trait Correlation",value=0,plotOutput("correlations") %>% withSpinner()),
+					tabPanel("Trait Histogram",value=1,plotOutput("histograms") %>% withSpinner()),
+					tabPanel("Trait Jitterplot",value=2,plotOutput("jitter") %>% withSpinner())
+				)
+			)
 		),
 		tabPanel("Rosette Experiment",
-				sidebarPanel(
-					fileInput("metadata_table",label = "Select file"),
-					div(style = "overflow-x:scroll;", tableOutput("meta")),
-					varSelectizeInput("file","Select column containing the filename", data = NULL),
-					splitLayout(cellArgs = list(style = "overflow:visible;"),
-						varSelectizeInput("date","Select column containing the date", data = NULL),
-						selectizeInput("date_format","Select format",choices=NULL)
-					),
-					varSelectizeInput("groupvar","Select column to group by", data = NULL),
-					actionButton("merge_data", "Merge data"),
-					selectizeInput("exp_traits","Select Trait:", choices = colnames(data %>% select(-file,-format)), selected = "class_norm_area")
+			sidebarPanel(
+				fileInput("metadata_table",label = "Select file"),
+				div(style = "overflow-x:scroll;", tableOutput("meta")),
+				varSelectizeInput("file","Select column containing the filename", data = NULL),
+				splitLayout(cellArgs = list(style = "overflow:visible;"),
+					varSelectizeInput("date","Select column containing the date", data = NULL),
+					selectizeInput("date_format","Select format",choices=NULL)
 				),
-				mainPanel(
-					tabsetPanel(id='tabset3',type='pills',
-								tabPanel("Traits over time",value=0,plotOutput("timeline")%>% withSpinner())
-					)
+				varSelectizeInput("groupvar","Select column to group by", data = NULL),
+				actionButton("merge_data", "Merge data"),
+				selectizeInput("exp_traits","Select Trait:", choices = colnames(data %>% select(-file,-format)), selected = "class_norm_area")
+			),
+			mainPanel(
+				tabsetPanel(id='tabset3',type='pills',
+							tabPanel("Traits over time",value=0,plotOutput("timeline") %>% withSpinner())
 				)
+			)
 		)
 )
 
 server <- function(input, output, session) {
 		# nextflow report is only generated after the run has finished once, show the tab only for resumed runs
 		if (dir.exists("www")) {
-		appendTab("nav",
-			tabPanel("Nextflow Report",
-			tabsetPanel(id='tabset4',type='pills',
-				tabPanel("Execution Report",value=0,htmlOutput("nf_report")),
-				tabPanel("Timeline",value=0,htmlOutput("nf_timeline"))
+			appendTab("nav",
+				tabPanel("Nextflow Report",
+					tabsetPanel(id='tabset4',type='pills',
+						tabPanel("Execution Report",value=0,htmlOutput("nf_report")),
+						tabPanel("Timeline",value=0,htmlOutput("nf_timeline"))
+					)
 				)
 			)
-		)
 		}
 		# for large datasets it helps with performance if selection lists are done on the server-side
 		updateSelectizeInput(session, "statistics_files", choices = c(imagenames), server = TRUE)
 		updateSelectizeInput(session, "explorer_files", choices = c(imagenames), server = TRUE)
 
-		output$mask <- renderImage({
+		output$mask <- renderImage(deleteFile=FALSE,{
 			list(src = glue::glue("diagnostics/mask/mask_{input$explorer_files}.png"),width=400,height=400)
-		}, deleteFile = FALSE)
-		output$hull <- renderImage({
+		})
+		output$hull <- renderImage(deleteFile=FALSE,{
 			list(src = glue::glue("diagnostics/convex_hull/hull_{input$explorer_files}.png"),width=400,height=400)
-		}, deleteFile = FALSE)
-		output$rosette <- renderImage({
+		})
+		output$rosette <- renderImage(deleteFile=FALSE,{
 			list(src = glue::glue("diagnostics/crop/crop_{input$explorer_files}.jpeg"),width=400,height=400)
-		}, deleteFile = FALSE)
-		output$overlay <- renderImage({
+		})
+		output$overlay <- renderImage(deleteFile=FALSE,{
 			list(src = glue::glue("diagnostics/overlay/overlay_{input$explorer_files}.jpeg"),width=400,height=400)
-		}, deleteFile = FALSE)
+		})
 		output$radar = renderChartJSRadar({
 			data %>%
 				filter(file == input$explorer_files) %>%
@@ -115,14 +115,14 @@ server <- function(input, output, session) {
 				mutate(value=value/sum(value)*100) %>% 
 				chartJSRadar(.,maxScale = 100,scaleStartValue = 0,scaleStepWidth = 25,showLegend = F)
 		})
-		output$histograms = renderPlot(width=600,height=600,{
+		output$histograms <- renderPlot(width=600,height=600,{
 			data %>%
 				select(file,input$statistics_traits) %>%
 				ggplot(aes_string(input$statistics_traits)) +
 				geom_histogram(bins = 100) +
 				theme_bw()
 		})
-		output$jitter = renderPlot(width=600,height=600,{
+		output$jitter <- renderPlot(width=600,height=600,{
 			data %>%
 				select(file,input$statistics_traits) %>%
 				ggplot(aes_string(x=as.factor(input$statistics_traits),y=input$statistics_traits)) +
@@ -132,13 +132,13 @@ server <- function(input, output, session) {
 				ylab("measurement") +
 				theme_bw()
 		})
-		output$correlations = renderPlot(width=600,height=600,{
+		output$correlations <- renderPlot(width=600,height=600,{
 			data %>%
 				select(-file,-format,-total_area) %>%
 				cor(use=input$correlations_type) %>%
 				corrplot(method="shade",tl.cex=0.5,tl.col="black",type="upper")
 		})
-		output$meta = renderTable(striped = TRUE,width="100px",{
+		output$meta <- renderTable(striped = TRUE,width="100px",{
 			filedata() %>% head(n=1)
 		})
 
@@ -161,18 +161,17 @@ server <- function(input, output, session) {
 				mutate(date = !!input$date %>% lubridate::parse_date_time(.,orders = input$date_format)) %>% 
 				group_by(!!input$groupvar)
 		})
-		output$timeline = renderPlot({
+		output$timeline <- renderPlot({
 			joined() %>% ggplot(aes_string(x='date',y=input$exp_traits,colour=quo(as.factor(!!input$groupvar)))) +
 				stat_summary() +
 				scale_color_viridis_d() +
 				theme_bw()
 		})
 
-		output$info = renderText({
+		output$info <- renderText({
 			glue::glue("Measured {traitcount} traits across {imagecount-invalid} rosettes ({invalid} failure(s))")
 		})
 		output$slickr <- renderSlickR({
-			
 			# split the filenames into chunks of 60, corresponding to 10 pages per chunk
 			# this drastically improves page loading time
 			chunks <- if (imagecount > 60) split(imagenames$file, ceiling(seq_along(imagenames$file)/60)) else split(imagenames$file, 1)
